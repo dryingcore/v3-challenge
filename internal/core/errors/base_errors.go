8b37e4d9d1a8e6d74ff568286f295148fd8db5c1
Gabriel <protonmail.default.sense07@protonmail.com>
2025-06-18 10:08:02 -0300

    modified: base_errors now have ErrTimestampIsEmpty

--- internal/core/errors/base_errors.go
+++ internal/core/errors/base_errors.go
@@ -5,4 +5,5 @@ import "errors"
 var (
 	ErrEmptyMacAddress   = errors.New("mac address must not be empty")
 	ErrTimestampInFuture = errors.New("timestamp cannot be in the future")
+	ErrTimestampIsEmpty  = errors.New("timestamp must not be empty")
 )
