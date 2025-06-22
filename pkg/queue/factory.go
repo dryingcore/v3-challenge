28bbf0e0c1e88e88444ffdcca05b36b67c1560e7
Gabriel <protonmail.default.sense07@protonmail.com>
2025-06-22 20:08:03 -0300

    modified: factory

--- pkg/queue/factory.go
+++ pkg/queue/factory.go
@@ -5,3 +5,7 @@ import core "github.com/dryingcore/v3-challenge/internal/core/ports/queue"
 func NewFakePublisher() core.Publisher {
 	return &DummyPublisher{}
 }
+
+func NewRealPublisher(url string) (core.Publisher, error) {
+	return NewNatsPublisher(url)
+}
